bfe8d82c088bf27a4352311d36fe43dc730f6c4e
EllaKaye <ella.kaye@gmail.com>
2025-11-27 10:21:15 +0000

    check_arg function

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -12,15 +12,17 @@
 #' animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound) {
 
-	if (!rlang::is_character(animal, n = 1)) {
-		cli::cli_abort("`animal` must be a single string")
-	}
-
-	if (!rlang::is_character(sound, n = 1)) {
-		cli::cli_abort("`sound` must be a single string")
-	}
+	check_arg(animal)
+	check_arg(sound)
 
 	paste0("The ", animal, " says ", sound, "!")
 }
 
+check_arg <- function(arg, n = 1) {
+	if (!rlang::is_character(arg, n = n)) {
+		cli::cli_abort(c("{.var {rlang::caller_arg(arg)}} must be a character vector of length {n}",
+										 "i" = "It was {.type {arg}} of length {length(arg)} instead."),
+									 call = rlang::caller_env())
+	}
+}
 
