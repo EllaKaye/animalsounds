bdda54d06e28efb2bd53cc137f14ae4e8233aba6
Ella Kaye (Advanced Research Computing) <e.kaye.1@bham.ac.uk>
2026-06-01 11:39:22 +0100

    Add documentation for animal_sounds

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,15 @@
+#' Print the Sound an Animal Makes
+#'
+#' Prints the sound that an animal makes.
+#'
+#' @param animal A string
+#' @param sound A sting
+#'
+#' @returns A string with the animal and sound
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
