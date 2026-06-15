0f9ba99dad9b5ee375cefe6f42c998f86feb4f6b
Pao Corrales <paobcorrales@gmail.com>
2026-06-15 21:02:08 +1000

    Add data to package

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Letters of the Alphabet with indices
+#'
+#' A dataset of lower-case letters
+#'
+#' @format A data frame with 26 rows and 2 variables:
+#' \describe{
+#'   \item{letter}{The letters of the alphabet}
+#'   \item{index}{The corresponding numberic index}
+#' }
+"letter_indices"
